686daa164c70f9d97e09539c667bd717b74ce141
Agnes Park <agnespark824@gmail.com>
2020-02-07 06:09:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,11 +2,16 @@ class Transfer
   attr_accessor :status
   attr_reader :sender, :receiver, :amount
   
+  @@all
+  
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
     @status = "pending"
+    @@all << 
+    
+    @trans_instance << 
   end
   
   def valid?
@@ -19,7 +24,14 @@ class Transfer
   
   def execute_transaction
     if self.valid?
-      
+      self.sender.balance - amount
+      self.receiver.deposit(amount)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+    
   end
 
   
