c564e31cf584e80591732a66e9131551f6ca18ee
Agnes Park <agnespark824@gmail.com>
2020-02-07 05:46:25 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,21 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amount)
+    self.balance += amount
+  end
+  
+  def display_balance
+    puts "Your balance is $#{self.balance}."
+  end
+  
+  def valid?
+  
 end
